fb56149b3118f02e276245e8fdfa622fecb1002e
William Bishop <will.h.bishop@gmail.com>
2026-03-31 14:27:36 -0700

    broken extractor test

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -14,5 +14,9 @@ export interface GenerateOptions {
     pattern: string | string[];
     mapper: MapperFunction;
     cwd?: string;
+    /** Output directory for generated tests (default: 'tests') */
+    outDir?: string;
 }
+/** @deprecated Use GenerateOptions instead - maintained for backwards compatibility */
+export type Config = GenerateOptions;
 //# sourceMappingURL=types.d.ts.map
